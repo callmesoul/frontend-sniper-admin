ab55355e2d2a54ddaef91c19b046e411c3087e6b
Yibin Chen <chenyibin@qtone.cn>
2019-03-07 08:42:36 +0800

    添加预览图

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ let config={
     if (process.env.NODE_ENV === 'development') {//测试域名
       return "http://127.0.0.1:7001";
     } else {//正式域名
-       return "http://frontend-sniper.callmesoul.cn";
+       return "https://frontend-sniper.callmesoul.cn";
     }
   },
   "tokenName": "token"
